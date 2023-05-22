2789e236aa4fe2f06077206ec8857efd2141aada
luckyjp6 <luckyjp6.cs09@nycu.edu.tw>
2023-05-22 11:05:25 +0800

    for merge

--- hw6/http_3_0_client.py
+++ hw6/http_3_0_client.py
@@ -1,5 +1,8 @@
 import socket
 import threading
+import os
+import glob
+import xml.etree.ElementTree as ET
 # import json
 import math, time
 # from Utils import Frame
@@ -742,35 +745,49 @@ class QUICClient:
         self.pkt_num += 1
         return r
 
+def write_file_from_response(file_path, response):
+    if response:
+        print(f"{file_path} begin")
+        with open(file_path, "wb") as f:
+            while True:
+                content = response.get_stream_content()
+                if content is None:
+                    break
+                f.write(content)
+        print(f"{file_path} end")
+    else:
+        print("no response")
+     
+
 if __name__ == '__main__':
     client = HTTPClient()
 
-    # target_path = "./tutorials/target"
-    # response = client.get(url=f"127.0.0.1:8080/")
-    # file_list = []
-    # if response:
-    #     headers = response.get_headers()
-    #     if not headers:
-    #         exit()
-    #     if headers['content-type'] == 'text/html':
-    #         body = response.get_full_body()
-    #         if not body:
-    #             exit()
-    #         root = ET.fromstring(body.decode())
-    #         links = root.findall('.//a')
-    #         file_list = []
-    #         for link in links:
-    #             file_list.append(link.text) 
+    target_path = "./tutorials/target"
+    response = client.get(url=f"127.0.0.1:8080/")
+    file_list = []
+    if response:
+        headers = response.get_headers()
+        if not headers:
+            exit()
+        if headers['content-type'] == 'text/html':
+            body = response.get_full_body()
+            if not body:
+                exit()
+            root = ET.fromstring(body.decode())
+            links = root.findall('.//a')
+            file_list = []
+            for link in links:
+                file_list.append(link.text) 
+                
+    for file in glob.glob(os.path.join(target_path, '*.txt')):
+        os.remove(file)
 
-    # for file in glob.glob(os.path.join(target_path, '*.txt')):
-    #     os.remove(file)
-
-    # th_list = []
-    # for file in file_list:
-    #     response = client.get(f"127.0.0.1:8080/static/{file}")
-    #     th = threading.Thread(target=write_file_from_response, args=[f"{target_path}/{file}", response])
-    #     th_list.append(th)
-    #     th.start()
+    th_list = []
+    for file in file_list:
+        response = client.get(f"127.0.0.1:8080/static/{file}")
+        th = threading.Thread(target=write_file_from_response, args=[f"{target_path}/{file}", response])
+        th_list.append(th)
+        th.start()
         
-    # for th in th_list:
-    #     th.join()
+    for th in th_list:
+        th.join()    
